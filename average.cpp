148d8701040d96f053392dab2f7c1ce67a9611cc
aksh-200 <81736871+aksh-200@users.noreply.github.com>
2023-09-12 03:23:22 -0700

    Add files via upload

---
+++ average.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e;
+	cout<<"Enter marks";
+	cin>>a>>b>>c>>d>>e;
+	int sum=a+b+c+d+e;
+	float avg =(float)sum/5;
+	cout<<"avg ="<<avg;
+}
---
+++ circum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int r;
+	const float pi1=3.14f;
+	float coc;
+	cout<<"Enter radius of circle=";
+	cin>>r;
+	coc=2*pi1*r;
+	cout<<"circumference of circle="<<coc;
+	return 0;
+}
