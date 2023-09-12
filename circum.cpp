e8a88d198f405b9e3d87e9c84855752ca0b44775
Prathmeshm1206 <144794295+Prathmeshm1206@users.noreply.github.com>
2023-09-12 03:38:19 -0700

    Update circum.cpp circle
    change cir to circle

--- circum.cpp
+++ circum.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main(){
 	int r;
 	const float pi1=3.14f;
-	float cir;
+	float circle;
 	cout<<"Enter radius of circle=";
 	cin>>r;
 	cir=2*pi1*r;
-	cout<<"circumference of circle="<<cir;
+	cout<<"circumference of circle="<<circle;
 	return 0;
 }
